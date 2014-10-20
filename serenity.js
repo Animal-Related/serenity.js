5b11a13759709da98267a2c475e3c27353e170cd
Louis Pilfold <louis@lpil.uk>
2014-10-20 22:02:33 +0100

    BPM, CSS

--- serenity.js
+++ serenity.js
@@ -1,8 +1,12 @@
 (function() {
-  var headline,
-      description,
+  var bpm = 80,
+      beatMs = 60000 / bpm,
+      headline,
       articleUrl;
 
+  //
+  // Get the Daily Mail front page headline + description from RSS
+  //
   var queryMailHeadline = function queryMailHeadline() {
     var request = new XMLHttpRequest(),
         query = 'http://query.yahooapis.com/v1/public/yql?q=' +
@@ -13,39 +17,35 @@
     request.open('GET', query, true);
 
     request.onload = function() {
-      var article, hEl, pEl, aEl, body;
+      var article, hEl, aEl, body;
 
       if (request.status >= 200 && request.status < 400){
         article = JSON.parse(request.responseText)
           .query.results.item;
           
         headline = article.title.trim();
-        description = article.description.join('').trim();
         articleUrl = article.link.trim();
 
         hEl = document.createElement('h1');
         hEl.innerHTML =
           '<span>'+ headline.split('').join('</span><span>') + '</span>';
-        pEl = document.createElement('p');
-        pEl.innerHTML = description;
         aEl = document.createElement('a');
-        aEl.innerHTML = 'Go here to get more depressed';
+        aEl.innerHTML = 'Find more misery here...';
         aEl.href = articleUrl;
 
-        body = document.querySelector('body');
+        body = document.querySelector('article');
         body.innerHTML = '';
         body.appendChild(hEl);
-        body.appendChild(pEl);
         body.appendChild(aEl);
 
         dancingLights();
       } else {
-        document.querySelector('body')
+        document.querySelector('article')
           .innerHTML = 'The YQL server returned an error';
       }
     };
     request.onerror = function() {
-      document.querySelector('body')
+      document.querySelector('article')
         .innerHTML = 'There was an error performing the YQL request';
     };
 
@@ -53,6 +53,9 @@
   };
 
 
+  //
+  // One by one, momentarily highlight a char in the headline
+  //
   var dancingLights = function dancingLights() {
     var i = 0,
         ele = document.querySelector('h1');
@@ -61,10 +64,10 @@
       ele.children[i].className = 'highlight';
 
       var x = i;
-      setTimeout(function() { ele.children[x].className = ''; }, 800);
+      setTimeout(function() { ele.children[x].className = ''; }, beatMs * 2);
 
       i = ++i % headline.length;
-    }, 100);
+    }, beatMs);
   };
 
   queryMailHeadline();
